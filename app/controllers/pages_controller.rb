e71733a2a6822e249c1212a516ccd92ca7e6d90a
womesh <wcbhargav@gmail.com>
2017-02-17 01:38:43 +0000

    created rails application,installed bootstrap scss gem and devise gem,added username to devise, added posts table and modeling posts

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def profile
+    if(User.find_by_username(params[:id]))
+   @username = params[:id]
+   else
+      redirect_to root_path, :notice=> "user not found!"
+    end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]) )
+    @newPost = Post.new
+  end
+
+  def explore
+    @posts = Post.all
+  end
+
+  def home
+    @posts = Post.all
+  end
+end
