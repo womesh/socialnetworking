c088b4b73841d4c4c0e4fb2a2427005db2865869
womesh <wcbhargav@gmail.com>
2017-03-15 10:20:34 +0000

    creating posts with the help of post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,13 @@ class PostsController < ApplicationController
    def create
        
        @post = Post.new(post_params)
-       @post.user_id = current_user.id
+       @post.user_id = current_user.id #assinging posts to the users who created it
        respond_to do |f|
            if (@post.save)
-               f.html { redirect_to "", notice "Post Crreated"}
+               f.html { redirect_to "", notice: "Post Crreated" } # if post is saved then it shows
            else
-               f.html{ redirect_to "", notice "Error: Post Not Saved" }
-           end
+               f.html{ redirect_to "", notice: "Error: Post Not Saved" } # if post is not created or any error in it
+           end 
        end
    end
       
@@ -25,6 +25,4 @@ class PostsController < ApplicationController
           params.require(:post).permit(:user_id,:content)
       end
       
-    
-    
 end
